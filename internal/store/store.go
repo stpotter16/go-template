3a6d55173379f34796a2ddaad8a40183c090ba48
Sam Potter <spotter1642@gmail.com>
2026-04-22 10:01:29 -0400

    Add a starting store package

---
+++ internal/store/store.go
@@ -0,0 +1,16 @@
+package store
+
+import (
+	"context"
+	"errors"
+
+	"github.com/stpotter16/go-template/internal/types"
+)
+
+var ErrUserNotFound = errors.New("user not found")
+
+type Store interface {
+	// Users
+	GetUserByUsername(ctx context.Context, username string) (types.User, error)
+	CreateUser(ctx context.Context, username, passwordHash string, isAdmin bool) error
+}
