2a082993dc3a5ab86fa4238c1414a56a49ebf67a
Sam Potter <spotter1642@gmail.com>
2026-04-22 09:48:58 -0400

    Clean up routes file to include basics

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -24,31 +24,11 @@ func addRoutes(
 	// Views that need authentication
 	viewAuthRequired := middleware.NewViewAuthenticationRequiredMiddleware(sessionManager)
 	mux.Handle("GET /{$}", viewAuthRequired(indexGet(store, sessionManager)))
-	mux.Handle("GET /plan", viewAuthRequired(planGet(store, sessionManager)))
-	mux.Handle("GET /transactions", viewAuthRequired(transactionsGet(store)))
-	mux.Handle("GET /transactions/new", viewAuthRequired(transactionNewGet(store, sessionManager)))
-	mux.Handle("GET /transactions/import", viewAuthRequired(transactionImportGet()))
-	mux.Handle("GET /transactions/{id}", viewAuthRequired(transactionDetailGet(store)))
-	mux.Handle("GET /transactions/{id}/edit", viewAuthRequired(transactionEditGet(store)))
-	mux.Handle("GET /accounts", viewAuthRequired(accountsGet(store)))
-	mux.Handle("GET /accounts/new", viewAuthRequired(accountNewGet()))
-	mux.Handle("GET /settings", viewAuthRequired(settingsGet()))
 
 	// Auth
 	mux.HandleFunc("POST /login", loginPost(authenticator, sessionManager))
 
 	// Session authenticated API endpoints
 	apiAuthRequired := middleware.NewApiAuthenticationRequiredMiddleware(sessionManager)
-	mux.Handle("POST /plan/{id}/lock", apiAuthRequired(planLockPost(store)))
-	mux.Handle("POST /plan/{id}/items", apiAuthRequired(planItemCreatePost(store)))
-	mux.Handle("PUT /plan-items/{id}", apiAuthRequired(planItemUpdatePut(store)))
-	mux.Handle("DELETE /plan-items/{id}", apiAuthRequired(planItemDeletePost(store)))
-	mux.Handle("POST /transactions", apiAuthRequired(transactionCreatePost(store, sessionManager)))
-	mux.Handle("POST /transactions/import/preview", apiAuthRequired(transactionImportPreviewPost(store)))
-	mux.Handle("POST /transactions/import", apiAuthRequired(transactionImportPost(store, sessionManager)))
-	mux.Handle("PUT /transactions/{id}", apiAuthRequired(transactionUpdatePut(store)))
-	mux.Handle("DELETE /transactions/{id}", apiAuthRequired(transactionDeletePost(store)))
-	mux.Handle("POST /transactions/{id}/plan-item", apiAuthRequired(transactionPlanItemPost(store)))
-	mux.Handle("POST /accounts", apiAuthRequired(accountCreatePost(store)))
-	mux.Handle("DELETE /accounts/{id}", apiAuthRequired(accountDeletePost(store)))
+	mux.Handle("POST /items", apiAuthRequired(postItems(store)))
 }
