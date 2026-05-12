da41d00b633be20d2701665e45625a8c834d54b6
Sam Potter <spotter1642@gmail.com>
2026-05-12 09:37:01 -0400

    Add script to build release server

---
+++ dev-scripts/build-release-server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Fail on first error
+set -e
+
+# Fail on unset variable
+set -u
+
+# Echo commands
+set -x
+
+go build \
+    -ldflags "-s -w -linkmode external -extldflags '-static'" \
+    -tags sqlite_omit_load_extension \
+    -o ./release/go-template \
+    cmd/server/main.go
