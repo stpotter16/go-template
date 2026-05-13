0acd353be1c7d63c2c250ae2a5a5a9e927ebc17a
Sam Potter <spotter1642@gmail.com>
2026-05-12 22:04:53 -0400

    Clean up a few lingering references to COIN project

--- internal/handlers/sessions/cookies.go
+++ internal/handlers/sessions/cookies.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stpotter16/go-template/internal/cookies"
 )
 
-const SESSION_COOKIE = "X-COIN-SESSION"
+const SESSION_COOKIE = "X-GO-TEMPLATE-SESSION"
 const SESSION_COOKIE_TTL = 3600 * 24 * 30
 
 func (s SessionManger) readSessionCookie(r *http.Request) (string, error) {
